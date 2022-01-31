4a01086e61e676285f7881808800df1e8b01c2cb
Sebas Corral <sebascorralbrito@gmail.com>
2022-01-31 13:15:49 +0100

    initial and iteration1

---
+++ src/components/IdCard.jsx
@@ -0,0 +1,15 @@
+const IdCard = ({lastName, firstName, gender, height, birth, picture}) => {
+    return(
+        <div className="IdCard">
+            <img src={picture} className="picture" alt="picture" />
+            <div className="text">
+                <strong>First Name: </strong> {firstName} <br />
+                <strong>Last Name: </strong> {lastName} <br />
+                <strong>Gender: </strong> {gender} <br />
+                <strong>Height: </strong> {height/100} m <br />
+                <strong>Birth: </strong> {birth.toDateString()} <br />
+            </div>
+        </div>
+    )
+}
+export default IdCard;
